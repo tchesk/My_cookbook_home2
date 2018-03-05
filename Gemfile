0530c4124e85b8ead95967a666ccd1327ed458cf
Francisco Felix <franciscofelixs@gmail.com>
2018-03-05 19:10:35 -0300

    Usuário usa cozinhas cadastradas

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails'
   gem 'capybara'
+  
 end
 
 group :development do
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,19 +5,21 @@ class RecipesController < ApplicationController
 
   def new
     @recipes = Recipe.new()
+    @cuisine = Cuisine.all
   end
 
   def create
 
-    recipe_params = params.require(:recipe).permit(:title, :recipe_type, :cuisine,
+    recipe_params = params.require(:recipe).permit(:title, :recipe_type, :cuisine_id,
       :difficulty, :cook_time, :ingredients, :method)
 
 
     @recipes = Recipe.new(recipe_params)
 
-    @recipes.save
+    @recipes.save!
 
     redirect_to recipe_path(@recipes.id)
+
   end
 
 
