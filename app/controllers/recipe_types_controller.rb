a9563bd3639f53cb0fc74ee4938a84033709f48a
Francisco Felix dos Santos <felix@Franciscos-MacBook-Air.local>
2018-03-13 15:03:26 -0300

    type has no recipe

--- app/controllers/recipe_types_controller.rb
+++ app/controllers/recipe_types_controller.rb
@@ -1,5 +1,11 @@
 class RecipeTypesController < ApplicationController
   def show
     @recipe_type = RecipeType.find(params[:id])
+    @recipe = Recipe.all
+
+    if @recipe.save
+    else
+      flash[:errors] = 'Nenhuma receita encontrada para este tipo de receitas'
+    end
   end
 end
