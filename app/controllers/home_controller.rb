2c0ba55c4d87b9a56063bb76981a50d0179f80fb
Francisco Felix dos Santos <felix@Franciscos-MacBook-Air.local>
2018-03-14 16:30:25 -0300

    fix link recipe_type without recipe

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @recipes = Recipe.all
     @cuisines = Cuisine.all
+    @recipe_types = RecipeType.all
   end
 end
--- app/controllers/recipe_types_controller.rb
+++ app/controllers/recipe_types_controller.rb
@@ -1,9 +1,10 @@
 class RecipeTypesController < ApplicationController
   def show
     @recipe_type = RecipeType.find(params[:id])
-    @recipe = Recipe.all
+    @recipe = Recipe.new
 
     if @recipe.save
+      redirect_to recipe_path(recipe.id)
     else
       flash[:errors] = 'Nenhuma receita encontrada para este tipo de receitas'
     end
