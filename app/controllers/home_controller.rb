677c6908478ed250b5b8b8feb1846a6c2f674359
Francisco Felix dos Santos <felix@Franciscos-MacBook-Air.local>
2018-03-15 15:52:29 -0300

    resolving conflict link recipe_type

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @recipes = Recipe.all
-    @cuisines = Cuisine.all
     @recipe_types = RecipeType.all
+    @cuisines = Cuisine.all
   end
 end
