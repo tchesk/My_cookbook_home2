e4ec38c5de8f3d4177cc3f1d1970edfdd3955f99
Francisco Felix dos Santos <felix@Franciscos-MacBook-Air.local>
2018-03-16 18:48:43 -0300

    admin register recipe type

--- app/controllers/recipe_types_controller.rb
+++ app/controllers/recipe_types_controller.rb
@@ -2,11 +2,24 @@ class RecipeTypesController < ApplicationController
   def show
     @recipe_type = RecipeType.find(params[:id])
     @recipe = Recipe.new
-
     if @recipe.save
       redirect_to recipe_path(recipe.id)
     else
       flash[:errors] = 'Nenhuma receita encontrada para este tipo de receitas'
+    end
+  end
+  def new
+    @recipe_type = RecipeType.new
+  end
+  def create
+    recipe_type = params.require(:recipe_type).permit(:name)
+    @recipe_type = RecipeType.new(recipe_type)
+
+    if @recipe_type.save
+      redirect_to recipe_type_path(@recipe_type.id)
+    else
+      flash[:errors] = @recipe_type.errors.full_messages
+      render :new
     end
   end
 end
--- app/models/recipe_type.rb
+++ app/models/recipe_type.rb
@@ -1,3 +1,4 @@
 class RecipeType < ApplicationRecord
   has_many :recipes
+  validates :name, presence: {message:'Você deve informar o nome do tipo de receita'}
 end
