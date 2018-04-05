66517ee37f02aeff8817f391f44535a8f154a848
Francisco Felix <franciscofelixs@gmail.com>
2018-04-05 17:14:41 -0300

    autentication config

--- spec/features/autentication_user_spec.rb
+++ spec/features/autentication_user_spec.rb
@@ -4,12 +4,11 @@ feature 'user login and show welcome message' do
   scenario '' do
     user = User.create(email: 'joao@email.com', password:'123456')
 
-
     visit root_path
-    click_on 'Entra na minha conta'
+    click_on 'login'
 
-    fill_in 'Email', text: 'joao@email.com'
-    fill_in 'Password', text: '123456'
+    fill_in 'email', with: 'joao@email.com'
+    fill_in 'password', with: '123456'
     click_on 'Logar'
     expect(current_path).to eq(root_path)
     expect(page).to have_css('p', text: "Seja bem-vindo #{email}")
